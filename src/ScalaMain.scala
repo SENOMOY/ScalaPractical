43610683e993f9e3011f83982f1dd6c646eb2c7f
SENOMOY <subhajitsen220188@gmail.com>
2017-04-12 17:44:07 +0530

    All

--- src/ScalaMain.scala
+++
@@ -1,9 +0,0 @@
-/**
-  * Created by senomoy on 25-08-2016.
-  */
-object ScalaMain {
-  def main(args: Array[String]): Unit = {
-    println("My first scala program with main method")
-  }
-}
-
--- src/SealedClass.scala
+++
@@ -1,9 +0,0 @@
-/**
-  * Created by subhajit.s on 8/26/2016.
-  */
-
-sealed trait SealedTrait { val b =1 }
-
-object SealedClass extends App with SealedTrait {
-  println("SubClass of SealedClass "+b)
-}
