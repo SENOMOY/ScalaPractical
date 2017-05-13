e82af2c3b82dd291ba383c7c1905491164a97cab
SENOMOY <subhajitsen220188@gmail.com>
2017-05-13 22:54:53 +0530

    Generic Function

---
+++ src/main/scala/GenericFunction.scala
@@ -0,0 +1,18 @@
+
+/**
+  * Created by subhajit.s on 13-05-2017.
+  */
+object GenericFunction extends App {
+
+  def genericFunction[T](item: T) = {
+
+    item match {
+      case st: String => println(s"It's String $st")
+      case int: Int => println(s"It's an Integer $int")
+      case dbl: Double => println(s"I's a Double $dbl")
+      case _ => println("Not the type we are expecting")
+    }
+  }
+
+  genericFunction(2f)
+}
