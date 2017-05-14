3c45ad49859a4d17bcc48def1b994fbbbe15caff
SENOMOY <subhajitsen220188@gmail.com>
2017-05-14 11:10:07 +0530

    Higher order function new updates

--- src/main/scala/HigherOrder.scala
+++ src/main/scala/HigherOrder.scala
@@ -1,13 +1,17 @@
 /**
   * Created by senomoy on 03-12-2016.
   */
-object HigherOrder extends App{
+object HigherOrder extends App {
 
-  def highFunc(fn: Int => String,num: Int) = fn(num)
+  def highFunc(fn: Any => String, num: Any) = fn(num)
 
   def func1[A](a: A) = "<" + a + ">"
 
   def func2[B](b: B) = "{" + b + "}"
 
-  println(highFunc(func2,10))
+  def func3[C](c: C) = "[" + c + "]"
+
+  println(highFunc(func1, 10))
+  println(highFunc(func2, 3.14))
+  println(highFunc(func3, "function 3"))
 }
